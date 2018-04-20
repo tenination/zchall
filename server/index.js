0e8de4116acb62f3e7f2f9fdd58b833533e91192
Tim Ninan <tim.ninan@gmail.com>
2018-04-19 21:14:42 -0500

    configured index.js file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-index.js
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
+});
+
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.log(`listening on port ${PORT}`);
+});
